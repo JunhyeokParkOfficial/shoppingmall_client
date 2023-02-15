3374053cd8a41f3dd076d59a94c0ee335eadbb85
junhyeok <junhyeok0246@naver.com>
2023-02-15 11:50:51 +0900

    관리자 페이지(상품목록 및 등록) 구현

--- src/HeaderAdmin.js
+++ src/HeaderAdmin.js
@@ -1,8 +1,47 @@
-const HeaderAdmin = () => {
+import { Link, useNavigate } from 'react-router-dom';
+import './css/HeaderAdmin.css';
+
+const HeaderAdmin = ({login:login,setLogin:setLogin}) => {
+    const navigate = useNavigate();
+    const logoutClick = () =>{
+        localStorage.clear();console.log("logout!!");
+        navigate(`/`);
+    }
     return (
-       <div>
-            <h1>HeaderAdmin</h1>
+       <><div id="Header">
+            <div className="Nav">
+                <div className="Header">
+                    <div className="logo">
+                       SHOPPING MALL
+                    </div>
+                    <div onClick={logoutClick}className='logoutMenu'>
+                        <a href='/'>LOGOUT</a>
+                    </div>
+                </div>
+            </div>
        </div>
+       <div>
+       <div className="left_menu">
+           <ul>
+               <li>
+                   <div>
+                       <Link to='/admin/dashboard'>대시보드</Link>
+                   </div>
+               </li>
+               <li>
+                   <div>
+                       <Link to='/admin/order'>주문조회</Link>
+                   </div>
+               </li>
+               <li>
+                   <div>
+                       <Link to='/admin/product'>상품관리</Link>
+                   </div>
+               </li>
+           </ul>
+       </div>
+   </div></>
     )
 }
+
 export default HeaderAdmin;
--- src/routes/admin/ProductReg.js
+++ src/routes/admin/ProductReg.js
@@ -1,8 +1,83 @@
-const ProductReg = () => {
+import axios from "axios";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const ProductReg = () =>{
+    const [name,setName] = useState("");
+    const [price,setPrice] = useState();
+    const [stock, setStock] = useState();
+    const [status,setStatus] = useState("");
+    const [category,setCategory] = useState("CATEGORY1");
+    const onNameHandler =(event)=>{
+        setName(event.target.value);
+    }
+    const onPriceHandler =(event)=>{
+        setPrice(event.target.value);
+    }
+    const onStockHandler =(event)=>{
+        setStock(event.target.value);
+    }
+    const onStatusHandler =(event)=>{
+        setStatus(event.target.value);
+    }
+    const onCategoryHandler = (event) => {
+        setCategory(event.target.value);
+    }
+    const onRegClick = async () => {
+        const uri = "http://localhost:3001/product";
+        const data = {
+            "item_name": name,
+            "item_status": status,
+            "price": price,
+            "stock": stock,
+            "category":category
+        }
+        await axios.post(uri,data);
+    }
     return (
-       <div>
-            <h1>ProductReg</h1>
-       </div>
+        <div>
+            <h2>상품등록</h2>
+            <div style={{width:"70%"}}>
+                <table>
+                    <tr>
+                        <td>상품명</td>
+                        <td><input onChange={onNameHandler}type="text"/></td>
+                    </tr>
+                    <tr>
+                        <td>가격</td>
+                        <td><input onChange={onPriceHandler} type="text"/></td>
+                    </tr>
+                    <tr>
+                        <td>세부사항</td>
+                        <td><input type="text"/></td>
+                    </tr>
+                    <tr>
+                        <td>재고</td>
+                        <td><input onChange={onStockHandler} type="text"/></td>
+                    </tr>
+                    <tr>
+                        <td>상품 상태</td>
+                        <td><input onChange={onStatusHandler} type="text"/></td>
+                    </tr>
+                    <tr>
+                        <td>상품 카테고리</td>
+                        <td><select onChange={onCategoryHandler}>
+                                <option value="CATEGORY1">CATEGORY1</option>
+                                <option value="CATEGORY2">CATEGORY2</option>
+                                <option value="CATEGORY3">CATEGORY3</option>
+                            </select></td>
+                    </tr>
+                    <tr>
+                        <td>상품 이미지</td>
+                        <td><input type="file"/></td>
+                    </tr>
+                </table>
+                <div onClick={onRegClick}>
+                    <a href="/admin/product">상품 등록하기</a>
+                </div>
+            </div>
+        </div>
     )
 }
+
 export default ProductReg;
