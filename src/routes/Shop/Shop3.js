90de5743d447f7cc468149154db8228cd9f652d0
junhyeok <junhyeok0246@naver.com>
2023-02-15 12:04:02 +0900

    상품 목록 페이지 구현 : css 추가작성 필요함

--- src/routes/Shop/Shop3.js
+++ src/routes/Shop/Shop3.js
@@ -1,8 +1,64 @@
-const Shop3 = () => {
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import '../../css/showProduct.css';
+
+const Shop3 = () =>{
+    const [data,setData] = useState([]);
+    const uri = "http://localhost:3001/product";
+    const getProduct =() =>{
+      axios.get(uri)
+      .then((response)=>{
+        return response.data.filter((product)=>product.item_status==="판매 중"&&product.category==="CATEGORY3")}).then((data)=>setData(data));
+    }
+    useEffect(()=>{
+        getProduct();
+    },[]);
     return (
-       <div>
-            <h1>Shop3</h1>
-       </div>
+        <div className="product_container">
+            <div className="product_content">
+                <div className='category_list'>
+                    <h2 className='category_title'>SHOP</h2>
+                    <ul>
+                        <li >
+                            <Link to="/category/0">ALL</Link>
+                        </li>
+                        <li>
+                            <Link to="/category/1">CATEGORY1</Link>
+                        </li>
+                        <li>
+                            <Link to="/category/2">CATEGORY2</Link>
+                        </li>
+                        <li>
+                            <Link id="selected" to="/category/3">CATEGORY3</Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className='product_list'>
+                    <ul>
+                        {data.map((data)=> {
+                            return (
+                                <li>
+                                <a>
+                                    <div>
+                                    <img style={{width:"100%"}} src="https://thumbs.dreamstime.com/b/transparent-designer-must-have-fake-background-39672616.jpg"/>
+                                    </div>
+                                    <div>
+                                        <div>{data.item_name}</div>
+                                        <div>상품 설명</div>
+                                        <div>
+                                            <div>{data.price}원</div>
+                                        </div>
+                                    </div>
+                                </a>
+                            </li>
+                            )
+                        })}
+                    </ul>
+                </div>
+            </div>
+        </div>
     )
 }
+
 export default Shop3;
