94085a0c279ba9e7ff69065e6e429c5812093173
junhyeok <junhyeok0246@naver.com>
2023-02-25 17:06:24 +0900

    feat : refreshTokenAxios 추가

--- src/utils/CustomAxios.js
+++ src/utils/CustomAxios.js
@@ -4,11 +4,23 @@ import { useNavigate } from "react-router-dom";
 import { remove_userInfo } from "../store/authReducer";
 
 
+export const refreshTokenAxios = axios.create({
+  withCredentials: true,
+});
+
+refreshTokenAxios.interceptors.request.use((config) => {
+  const token = localStorage.getItem("accessToken");
+  if (config.headers !== undefined) {
+    if (token !== null) {
+      config.headers.Authorization = "Bearer " + token;
+    }
+  }
+  return config;
+});
+
+
+
 export const formDataAxios = axios.create({
-  //baseURL: "http://localhost:8080",
- // headers: {
-   // "Content-Type": "multipart/form-data",
-  //},
   withCredentials: true,
 });
 
@@ -25,15 +37,12 @@ formDataAxios.interceptors.request.use((config) => {
 formDataAxios.interceptors.response.use(
    (response) => response,
    async (error) => {
-    console.log("interceptor : ",error);
     const { config, response: { status } } = error;
 
     //1.refresh 요청자체의 에러인 경우
     //2.500 에러(토큰만료)가 아닌 경우
     //3. refresh하고 다시 요청을 보냈는데 에러인 경우
     if (config.url === "/api/auth/v1/reissue" || (status !== 500&&error.response.data.msg!=="인증이 실패하였습니다." ) || config.sent) {
-      console.log(config.url," ",status," ",config.sent," ",error.response.data.msg);
-      console.log("reject error!!");
       return Promise.reject(error);
   }
 
@@ -70,21 +79,17 @@ Axios.interceptors.request.use((config) => {
 Axios.interceptors.response.use(
    (response) => response,
    async (error) => {
-    console.log("interceptor : ",error);
     const { config, response: { status } } = error;
 
     //1.refresh 요청자체의 에러인 경우
     //2.500 에러(토큰만료)가 아닌 경우
     //3. refresh하고 다시 요청을 보냈는데 에러인 경우
     if (config.url === "/api/auth/v1/reissue" || (status !== 500&&error.response.data.msg!=="인증이 실패하였습니다." ) || config.sent) {
-      console.log(config.url," ",status," ",config.sent," ",error.response.data.msg);
-      console.log("reject error!!");
       return Promise.reject(error);
   }
-
     config.sent = true;
     const accessToken = await getRefreshToken();
-
+console.log(accessToken);
     if (accessToken) {
       config.headers.Authorization = "Bearer " + accessToken;
     }
@@ -96,22 +101,17 @@ Axios.interceptors.response.use(
 const getRefreshToken = async() => {
   const refreshToken = localStorage.getItem("refreshToken");
   const accessToken = localStorage.getItem("accessToken");
-  const data = {refreshToken:refreshToken,accessToken:accessToken};
-  console.log("getRefreshTOken : ",data);
-  await axios.post("/api/v1/auth/reissue",data)
+  const data = {"refreshToken":refreshToken,"accessToken":accessToken};
+  await refreshTokenAxios.post("/api/v1/auth/reissue",data)
     .then((res) => {
-      console.log("reissue then : ",res);
       const response = res.data;
       const newAccessToken = response.accessToken;
       localStorage.removeItem("accessToken");
       localStorage.setItem("accessToken", newAccessToken);
     })
     .catch((e) => {
-      console.log("getRefreshTOken Error");
-      console.log(e.response);
       const dispatch = useDispatch();
       const navigate = useNavigate();
-      alert("로그아웃되었습니다");
       console.log("Token Reissue Fail : " + e);
       localStorage.removeItem('accessToken');
       localStorage.removeItem('refreshToken');
