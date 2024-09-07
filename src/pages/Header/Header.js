b52678f8d15910d63fb8133d2c824043c60993aa
jhpark1227 <junhyeok0246@naver.com>
2024-09-07 13:12:28 +0900

    feat : 내비게이션 바 디자인 변경

--- src/pages/Header/Header.js
+++ src/pages/Header/Header.js
@@ -3,82 +3,93 @@ import { Link } from 'react-router-dom';
 import { Axios } from '../../utils/CustomAxios';
 import { remove_userInfo } from '../../store/authReducer';
 import { useCookies } from 'react-cookie';
+import { useState } from 'react';
+import categoryIcon from '../../assets/category.png'
+import logo from '../../assets/logo.png'
+import cartIcon from '../../assets/cart.png'
+import userIcon from '../../assets/user.png'
 const Header = () => {
     const dispatch = useDispatch();
     const user = useSelector(state=>state.user);
     const [cookies, setCookie, removeCookie] = useCookies();
+    const [isHovering, setIsHovering] = useState(false);
 
-    const logoutClick = () =>{
+    const logout = () =>{
         dispatch(remove_userInfo());
         localStorage.removeItem("accessToken");
         removeCookie('refreshToken');
         const uri="api/v1/member/logout";
         Axios.get(uri);
     }
+
+    const navigateToLogin = () => {
+        window.location.href = "/login"
+    }
+
+    const handleMouseOver = () => {
+        setIsHovering(true);
+    };
+
+    const handleMouseOut = () => {
+        setIsHovering(false);
+    };
+
     return (
-       <div id="Header">
-            <div className="Nav">
-                <div className="Header">
-                    <div className="logo">
-                        <Link to="/">UOU-MALL</Link>
-                    </div>
-                    <div className="menu_list">
-                        <ul className="menu">
-                            <li className="drop">
-                                <p>
-                                    <Link to="/category/0/1">SHOP</Link>
-                                </p>
-                                {/*<ul className="sub sub1">
-                                    <div className="inner">
-                                        <ul>
-                                            <p>
-                                                <a href='/category/0/1'>ALL</a>
-                                            </p>
-                                        </ul>
-                                    </div>
-                                </ul>*/}
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="membership_list">
-                        <ul className="menu">
-                            <li className="drop">
-                                MEMBERSHIP
-                                <ul className="sub sub2">
-                                    <div className="inner">
-                                        <ul>
-                                            {user.isLoggedIn?(
-                                                <div>
-                                                    <p >
-                                                        <a onClick={logoutClick} href="/">LOGOUT</a>
-                                                    </p>
-                                                    <p>
-                                                        <Link to='/mypage/order/1'>MY PAGE</Link>
-                                                    </p>
-                                                </div>
-                                                ):(
-                                                <div>
-                                                    <p>
-                                                        <Link to='/login'>LOGIN</Link>
-                                                    </p>
-                                                    <p>
-                                                        <Link to='/register'>REGISTER</Link>
-                                                    </p>
-                                                </div>
-                                                )}
-                                            
-                                        </ul>
-                                    </div>
-                                </ul>
-                            </li>
-                            <li>
-                                {user.isLoggedIn?<a href='/cart/1'>CART</a>:<a href='/login'>CART</a>}
-                            </li>
-                        </ul>
-                    </div>
+        <div id="header-container">
+            <div id="header">
+                <div id="header-top">
+                    {user.isLoggedIn?
+                         <button onClick={logout}>로그아웃</button>
+                        :
+                        <button onClick={navigateToLogin}>로그인/회원가입</button>
+                    }
                 </div>
+                <div id="header-mid">
+                    <div id="logo-box">
+                        <Link to="/">
+                            <img src={logo} style={{height:'50px'}}/>
+                        </Link>
+                    </div>
+                    <div id='search-box'>
+                        <input id='search-input' placeholder='검색어를 입력해 주세요.'></input>
+                    </div>
+                    <div id='button-box'>
+                        <a onClick={navigateToLogin}>
+                            <img src={cartIcon} style={{marginRight:'7px'}}/>장바구니
+                        </a>
+                        <a onClick={navigateToLogin} style={{marginRight:'5px'}}>
+                            <img src={userIcon} style={{marginRight:'3px'}}/>마이페이지
+                        </a>
+                    </div>
+                </div>  
+                <div id='header-bottom'>
+                    <div id='category-box' onMouseOver={handleMouseOver}>
+                        <button>
+                            <img src={categoryIcon} />
+                            카테고리
+                        </button>
+                        {isHovering?
+                            (<div id='dropdown-menu-box'>
+                                <ul>
+                                    <li className='category-item-box'>
+                                        <a href='/product?category=clothing'>의류</a>
+                                    </li>
+                                    <li className='category-item-box'>
+                                        <a >의류</a>
+                                    </li>
+                                    <li className='category-item-box'>
+                                        <a >의류</a>
+                                    </li>
+                                    <li className='category-item-box'>
+                                        <a >의류</a>
+                                    </li>
+                                </ul>
+                            </div>)
+                            :(<></>)}
+                    </div>
+                </div>  
             </div>
-       </div>
+        </div>
     )
 }
 
