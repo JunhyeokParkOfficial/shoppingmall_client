056df93ac47fa0e7f108b1a322158194810bf5a1
jhpark1227 <junhyeok0246@naver.com>
2024-09-23 22:15:40 +0900

    feat : 상품 관리 디자인 수정 및 styled-components 적용

---
+++ src/pages/admin/orderManage/OrderManage.style.js
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+
+export const AdminContentContainer = styled.div`
+    float: right;
+    width: 70%;
+    margin-top: 80px;
+    margin-right: 50px;
+    padding: 0px 30px 200px;
+`
+
+export const AdminContentTitle = styled.div`
+    font-size: 30px;
+    margin-bottom: 20px
+`
+
+export const OrderManageTable = styled.div`
+    border-bottom: solid 1px black;
+`
+
+export const OrderManageTopTR = styled.div`
+    display: table;
+    width: 100%;
+    table-layout: fixed;
+    border-top: 4px solid rgb(0, 0, 0);
+`
+
+export const OrderManageLargeTH = styled.div`
+    display: table-cell;
+    height: 74px;
+    line-height: 24px;
+    font-size: 18px;
+    font-weight: 700;   
+    text-align: center;
+    vertical-align: middle;
+    width: 150px;
+`
+
+export const OrderManageSmallTH = styled.div`
+    display: table-cell;
+    height: 74px;
+    line-height: 24px;
+    font-size: 18px;
+    font-weight: 700;   
+    text-align: center;
+    vertical-align: middle;
+    width: 15%;
+`
---
+++ src/pages/admin/productManage/ProductManage.style.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    float: right;
+    width: 70%;
+    margin-top: 80px;
+    margin-right: 50px;
+    padding: 0px 30px;
+`
+
+export const Title = styled.div`
+    font-size: 30px;
+    margin-bottom: 20px;
+`
