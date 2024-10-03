a1ab353e94f2a6adacfd6d06229e6d5a9f391fb6
jhpark1227 <junhyeok0246@naver.com>
2024-10-03 14:51:43 +0900

    design : 상품 등록 디자인 변경

---
+++ src/components/MypageLayout/MypageLayout.style.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const MypageContainer = styled.div`
+    display: flex;
+`
+
+export const MypageContentDiv = styled.div`
+    margin-right: 100px;
+    margin-left: 40px;
+    margin-top: 30px;
+    margin-bottom: 60px;
+    width: 1024px;
+`
+
+export const Title = styled.h1`
+    border-bottom: solid;
+    padding-bottom: 21px;
+`
---
+++ src/pages/ProductReg/ProductReg.style.js
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+export const ProductRegButton = styled.button`
+    width: 150px;
+    height: 70px;
+    font-size: 17px;
+    margin-top: 15px;
+    float: right;
+    margin-right: 100px;
+`
+
+export const ProductRegTable = styled.table`
+    margin-left: 100px;
+    margin-top: 30px;
+`
+
+export const ProductRegTd = styled.td`
+    border-bottom: 1px solid #e0e0e0;
+    padding: 20px;
+    font-weight: 500;
+    text-align: left;
+    width: 100px;
+`
+
+export const NameInput = styled.input`
+    width: 300px;
+    padding: 10px;
+`
+
+export const PriceInput = styled.input`
+    width: 200px;
+    padding: 10px;
+    margin-right: 5px;
+`
+
+export const DetailTextarea = styled.textarea`
+    width: 650px;
+    height: 200px;
+`
